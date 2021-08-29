76b36abab83f6423026819a6f28dbf938ee06ae9
JOHN DAVID BARRERA ZAPATA <johnbarrera214888@correo.itm.edu.co>
2021-08-29 00:27:17 -0500

    Add entity

---
+++ tododavid.Common/Responses/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace tododavid.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object MyProperty { get; set; }
+    }
+}
